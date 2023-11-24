3d75c2f646f0bd79d071cdf415f3a17c0eaa6d2a
Fatemeh askari <askarifatemeh96@gmail.com>
2023-11-24 14:28:12 +0330

    feat: add model and constant

---
+++ src/app/constants/constants.ts
@@ -0,0 +1,8 @@
+export const keypadKeys = [
+  ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
+  ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
+  ["Enter", "z", "x", "c", "v", "b", "n", "m", "Backspace"],
+];
+
+export const WORD_LENGTH = 5;
+export const POSSIBLE_WORD_COUNT = 6;
---
+++ src/app/models/models.ts
@@ -0,0 +1,2 @@
+export type LetterStatus = "hit" | "miss" | "match" | "none";
+export type GameStatus = "win" | "lose" | "playing";
